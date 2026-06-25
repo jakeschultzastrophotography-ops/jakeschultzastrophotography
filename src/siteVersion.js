7fbfe319c107665efcbf856fb3b36a1e6f390bf4
Jake Schultz <jakeschultzastrophotography@gmail.com>
2026-06-25 12:43:01 -0500

    Release v2.7

--- src/siteVersion.js
+++ src/siteVersion.js
@@ -1,4 +1,4 @@
-export const SITE_VERSION_NUMBER = "2.6.40";
+export const SITE_VERSION_NUMBER = "2.7";
 export const SITE_VERSION = `v${SITE_VERSION_NUMBER}`;
-export const RELEASE_NAME = "2.6.40 final image template polish";
+export const RELEASE_NAME = "2.7 release image template";
 export const RELEASE_DATE = "2026-06-25";

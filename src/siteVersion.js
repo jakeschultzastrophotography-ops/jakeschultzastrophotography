1efff124a2d85bec1b957876b46ca443afb9703e
Jake Schultz <jakeschultzastrophotography@gmail.com>
2026-06-26 14:35:56 -0500

    Release v2.8.36

--- src/siteVersion.js
+++ src/siteVersion.js
@@ -1,4 +1,4 @@
-export const SITE_VERSION_NUMBER = "2.8.35";
+export const SITE_VERSION_NUMBER = "2.8.36";
 export const SITE_VERSION = `v${SITE_VERSION_NUMBER}`;
-export const RELEASE_NAME = "2.8.35 embedded tool clean centering";
+export const RELEASE_NAME = "2.8.36 mobile astrodepth professional polish";
 export const RELEASE_DATE = "2026-06-26";
